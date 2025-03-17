f95e54d3dc3d9de6d352992671f860da10cfb17d
gayalsanjana <gayalranchagoda@gmail.com>
2025-03-17 12:32:43 +0530

    change some codes in catogry controller

--- src/main/java/com/example/NewsWebSite/controller/CategoryController.java
+++ src/main/java/com/example/NewsWebSite/controller/CategoryController.java
@@ -14,25 +14,21 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
-    // Endpoint to get all categories
-    @GetMapping
+   @GetMapping
     public List<Category> getAllCategories() {
         return categoryService.getAllCategories();
     }
 
-    // Endpoint to get a category by ID
-    @GetMapping("/{id}")
+   @GetMapping("/{id}")
     public Category getCategoryById(@PathVariable Long id) {
         return categoryService.getCategoryById(id);
     }
 
-    // Endpoint to add a new category
-    @PostMapping
+  @PostMapping
     public Category addCategory(@RequestBody Category category) {
         return categoryService.addCategory(category);
     }
 
-    // Endpoint to delete a category by ID
     @DeleteMapping("/{id}")
     public void deleteCategory(@PathVariable Long id) {
         categoryService.deleteCategory(id);
